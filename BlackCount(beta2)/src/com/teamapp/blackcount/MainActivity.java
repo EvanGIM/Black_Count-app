d69b841d1c538ef419d1df04f7631d51a8342478
EvanGIM <knya1234@gmail.com>
2014-08-05 15:14:44 +0900

    Team_Black_Count
    (beta2)

---
+++ BlackCount(beta2)/src/com/teamapp/blackcount/MainActivity.java
@@ -0,0 +1,77 @@
+package com.teamapp.blackcount;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.app.*;
+import android.view.View.OnClickListener.*;
+import android.widget.Button.*;
+import android.content.*;
+import android.net.*;
+import android.app.Activity.*;
+import android.media.MediaPlayer.*;
+import android.os.Bundle.*;
+import android.app.Activity.*;
+import android.app.AlertDialog.*;
+import android.content.DialogInterface.*;
+import android.os.Bundle.*;
+import android.view.View.*;
+import android.view.View.OnClickListener.*;
+import android.widget.Button.*;
+import android.webkit.*;
+import android.media.*;
+import android.widget.Toast.*;
+import android.app.Activity;
+import android.widget.EditText.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import android.app.Activity;
+import android.content.Context;
+import android.database.SQLException;
+import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Im;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.renderscript.*;
+import android.widget.*;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		
+	{Button Button1 = (Button) findViewById(R.id.button1);
+	
+	Button1.setOnClickListener(new OnClickListener()
+	 {
+	  public void onClick(View v)
+	  {
+	  Intent i=new Intent(MainActivity.this,Website.class);
+	  startActivity(i);
+	  }
+    });
+}}}
